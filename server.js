10d526b49ef55dc1f3b2975e61e33b1a644075fb
Ahutosh <as0326142@gmail.com>
2025-09-24 07:21:53 +0530

    Serve static files using sendFile method

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
 import express from 'express';
+import { open } from 'fs/promises'
 
 const app = express()
 
 const PORT = process.env.PORT || 4000
 
-//Parsing json body (Custom middleware)
-// app.use((req, res, next)=>{
-//     req.on("data", (chunk)=>{
-//         // console.log(JSON.parse(chunk.toString()))
-//         const reqBody = JSON.parse(chunk.toString())
-//         req.body = reqBody
-//         next()
-//     })
-// })
+// for serving static file from public folder
+app.use(express.static("public"))
 
-// express middleware
-app.use(express.json())
-
-// req.url === routeName
-// "/Home" === "/Home"
-
-// app.get('/Home', (req, res)=>{
-//     res.end('Home page')
-//     // res.send('Home page')
-// })
-
-// app.post('/', (req, res)=>{
-//     console.log(req.body)
-//     res.end('Home page')
-// })
-
-// req.url === routeName
-// "/users".startsWith("/users")
-// "/users" => one block
-// if "/users" comes in sec block with some additional block "/users/12" then also the first middleware will call bcz the first block of the both middleware were match so these will call first middleware instead of second
-
-app.use('/users/23', (req, res, next)=>{
-    res.send('First Middleware')
+app.get('/', (req, res)=>{
+    res.end('Second Middleware')
 })
-app.use('/users/12', (req, res, next)=>{
-    res.send('Second Middleware')
+app.get('/video',async(req, res)=>{
+//     const fileHandle = await open("hero.mp4");
+//   const readStream = fileHandle.createReadStream();
+//   const stats = await fileHandle.stat();
+//   res.setHeader("Content-Length", stats.size);
+//   res.setHeader("Content-Type", "video/mp4");
+//   res.setHeader("Accept-Ranges", "bytes");
+//   readStream.pipe(res);
+    res.sendFile(`${import.meta.dirname}/hero.mp4`)
 })
 
 app.listen(PORT, ()=>{
