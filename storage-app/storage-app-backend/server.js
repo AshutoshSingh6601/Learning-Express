26e70a441be9d49afb911b2af97a9d361d142af3
Ahutosh <as0326142@gmail.com>
2025-12-18 17:02:44 +0530

    handling file upload and cors

--- storage-app/storage-app-backend/server.js
+++ storage-app/storage-app-backend/server.js
@@ -1,31 +1,73 @@
 import express from 'express'
-import {readdir} from 'fs/promises'
+// import cors from 'cors'
+import { createWriteStream } from 'fs'
+// import { rm } from 'fs'
+import {readdir, rename, rm, stat} from 'fs/promises'
 
 const app = express()
 const port = 4000
 
-// Enabling cors
-app.use((req, res, next)=>{
-    res.set("Access-Control-Allow-Origin", "*")
-    next()
-})
+app.use(express.json())
 
-// app.use(express.static('storage'))
+// app.use(cors())
 
-// Serving Files
+// Enabling cors (Cross-Origin Resource sharing)
+// app.use((req, res, next)=>{
+//     // res.set("Access-Control-Allow-Origin", "*")
+//     // res.set("Access-Control-Allow-Methods", "*")
+//     res.set({
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Methods": "*",
+//         "Access-Control-Allow-Headers": "*",
+//     })
+//     next()
+// })
+
+// allow multiple origins
 app.use((req, res, next)=>{
-    if(req.query.action === 'download'){
-        res.set("Content-Disposition", "attachment")
+    const allowOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173/']
+    if(allowOrigins.includes(req.headers.origin)){
+        res.set('Access-Control-Allow-Origin',req.headers.origin)
     }
-    const serveStatic = express.static('storage')
-    serveStatic(req, res, next)
 })
 
-// Serving dir content
-app.get('/', async(req, res)=>{
-    const filesList = await readdir("./storage")
+// Read
+app.get('/directory', async(req, res)=>{
+    const filesList = await readdir('./storage')
     res.json(filesList)
 })
+// view image and download
+app.get('/files/:filename', (req, res)=>{
+    const { filename } = req.params
+    if(req.query.action === 'download'){
+        res.set("Content-Disposition", "Attachment")
+    }
+    res.sendFile(`${import.meta.dirname}/storage/${filename}`)
+})
+// create
+app.post('/files/:filename', (req, res)=>{
+    const writeStream = createWriteStream(`./storage/${req.params.filename}`)
+    req.pipe(writeStream)
+    req.on('end', ()=>{
+        res.json({message: 'File uploaded'})
+    })
+})
+// delete
+app.delete('/files/:filename', async(req, res)=>{
+    const { filename } = req.params
+    console.log(filename)
+    await rm(`${import.meta.dirname}/storage/${filename}`)
+    res.json({Message: "File Deleted Successfully"})
+})
+// update
+app.patch('/files/:filename', async(req, res)=>{
+    const { filename } = req.params
+    console.log(filename)
+    console.log(req.body)
+    await rename(`./storage/${filename}`, `./storage/${req.body.newFilename}`)
+    res.json({Message: "File Updated Successfully"})
+})
+
 
 app.listen(port, ()=>{
     console.log(`Server Listening on http://localhost:${port}`)
--- storage-app/storage-app-frontend/src/StoreFiles.jsx
+++ storage-app/storage-app-frontend/src/StoreFiles.jsx
@@ -4,6 +4,13 @@ const StoreFiles = () => {
 
     const [files, setFiles] = useState([])
 
+    const [isRename, setIsRename] = useState(false)
+
+    const [renamedValue, setRenamedValue] = useState({
+        oldName: '',
+        newName: ''
+    })
+
     const handleAllFiles = async() => {
         const resp = await fetch("http://localhost:4000", {
             method: "GET"})
@@ -12,17 +19,46 @@ const StoreFiles = () => {
         setFiles(data)
     }
 
+    const handleDeleteFiles = async(filename) => {
+        const resp = await fetch(`http://localhost:4000/${filename}`, {
+            method: "DELETE"})
+        const data = await resp.json()
+        console.log(data)
+        handleAllFiles()
+    }
+
+    const handleUpdateFiles = async(filename) => {
+        setRenamedValue({...renamedValue, newName: renamedValue.oldName})
+        const resp = await fetch(`http://localhost:4000/${renamedValue.oldName}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({newName: renamedValue.newName})
+        })
+        const data = await resp.json()
+        console.log('patch data',data)
+        console.log('patch data',renamedValue.newName)
+        handleAllFiles()
+    }
+
+    const handleRenameFiles = async(filename) => {
+        setIsRename(true)
+        setRenamedValue({...renamedValue, oldName: filename, newName: filename})
+    }
+
     useEffect(() => {
 handleAllFiles()
     }, [])
     
-
+    
   return (
     <div>
         <h1>My Files</h1>
         <p>Store and manage your files with ease.</p>
 
         <input type="file" />
+        {isRename && <input type="text" name='renamedValue' value={renamedValue.newName} onChange={(e)=>setRenamedValue({...renamedValue, newName: e.target.value})} />}
         <p>progress: 0%</p>
 {
     files.map((file, i) => (
@@ -30,9 +66,9 @@ handleAllFiles()
             <span>{file}</span>
             <a href={`http://localhost:4000/${file}?action=open`}>Open</a>
             <a href={`http://localhost:4000/${file}?action=download`}>Download</a>
-            <button>Rename</button>
-            <button>Save</button>
-            <button>Delete</button>
+            <button onClick={()=>handleRenameFiles(file)}>Rename</button>
+            <button onClick={()=>handleUpdateFiles(file)}>Save</button>
+            <button onClick={()=>handleDeleteFiles(file)}>Delete</button>
         </div>
     )
 )
