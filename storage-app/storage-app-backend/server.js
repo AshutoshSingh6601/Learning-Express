2a60864adbd28025e5277316c6b5d22f9cf834af
AshutoshSingh6601 <as0326142@gmail.com>
2026-01-24 21:52:12 +0530

    add multiple folder, fixed Path Traversal Vulnerability(PTV)

--- storage-app/storage-app-backend/server.js
+++ storage-app/storage-app-backend/server.js
@@ -1,15 +1,16 @@
-import express from 'express'
-import cors from 'cors'
-import { createWriteStream } from 'fs'
+import express from "express";
+import cors from "cors";
+import { createWriteStream } from "fs";
 // import { rm } from 'fs'
-import {mkdir, readdir, rename, rm, stat} from 'fs/promises'
+import { mkdir, readdir, rename, rm, stat } from "fs/promises";
+import path from "path";
 
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 // Enabling cors (Cross-Origin Resource sharing)
 // app.use((req, res, next)=>{
@@ -32,72 +33,81 @@ app.use(cors())
 // })
 
 // Read directory
-app.get('/directory/{*dirname}', async(req, res)=>{
-    const { dirname} = req.params
-    let filePath = dirname?.join('/')
-    // console.log('get filename', filePath)
-    const fullDirName = `./storage/${filePath? filePath : ''}`
-    const filesList = await readdir(fullDirName)
-    // console.log('filesList', filesList)
-    const resData = []
-    for(const item of filesList){
-        const stats = await stat(`${fullDirName}/${item}`)
-        resData.push({name: item, isDirectory: stats.isDirectory()})
+app.get("/directory/{*dirname}", async (req, res) => {
+  const { dirname } = req.params;
+  try {
+    let filePath = path.join("/", dirname ? dirname?.join("/") : '');
+    const fullDirName = `./storage/${filePath ? filePath : ""}`;
+    const filesList = await readdir(fullDirName);
+    const resData = [];
+    for (const item of filesList) {
+      const stats = await stat(`${fullDirName}/${item}`);
+      resData.push({ name: item, isDirectory: stats.isDirectory() });
     }
-    res.json(resData)
-})
+    res.json(resData);
+  } catch (error) {
+    res.json({error: error.message})
+  }
+});
 
-// create directory
-app.post('/directory/{*dirname}', async(req, res)=>{
-    const {dirname} = req.params
-    const filePath = dirname.join('/')
-    try {
-        await mkdir(`./storage/${filePath}`)
-        res.status(200).json({message: 'Directory created successfully'})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+// create new directory
+app.post("/directory/{*dirname}", async (req, res) => {
+  const { dirname } = req.params;
+  try {
+    let filePath = path.join("/", dirname ? dirname?.join("/") : '');
+    await mkdir(`./storage/${filePath}`);
+    res.status(200).json({ message: "Directory created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // view image and download
-app.get('/files/*dirname', (req, res)=>{
-     const { dirname } = req.params;
-     let filePath = dirname.join('/')
-    if(req.query.action === 'download'){
-        res.set("Content-Disposition", "Attachment")
-    }
-    res.sendFile(`${import.meta.dirname}/storage/${filePath}`)
-})
-// create
-app.post('/files/*dirname', (req, res)=>{
-    const { dirname } = req.params
-    let filename = dirname.join('/')
-    const writeStream = createWriteStream(`./storage/${filename}`)
-    req.pipe(writeStream)
-    req.on('end', ()=>{
-        res.json({message: 'File uploaded'})
-    })
-})
+app.get("/files/*dirname", (req, res) => {
+  const { dirname } = req.params;
+  let filePath = path.join("/", dirname ? dirname?.join("/") : '');
+  if (req.query.action === "download") {
+    res.set("Content-Disposition", "Attachment");
+  }
+  res.sendFile(`${import.meta.dirname}/storage/${filePath}`, (err)=>{
+    if(err)
+    res.json({error: err.message})
+  });
+});
+
+// create new file
+app.post("/files/*dirname", (req, res) => {
+  const { dirname } = req.params;
+  let filename = path.join("/", dirname ? dirname?.join("/") : '');
+  const writeStream = createWriteStream(`./storage/${filename}`);
+  req.pipe(writeStream);
+  req.on("end", () => {
+    res.json({ message: "File uploaded" });
+  });
+});
 // delete
-app.delete('/files/*dirname', async(req, res)=>{
-    try {
-        const { dirname } = req.params
-        let filename = dirname.join('/')
-        await rm(`${import.meta.dirname}/storage/${filename}`, {recursive: true})
-        res.status(200).json({Message: "File Deleted Successfully"})        
-    } catch (error) {
-        res.status(500).json({Message: "File Deletion Unsuccessfully"})        
-    }
-})
+app.delete("/files/*dirname", async (req, res) => {
+  const { dirname } = req.params;
+  try {
+    let filename = path.join("/", dirname ? dirname?.join("/") : '');
+    await rm(`${import.meta.dirname}/storage/${filename}`, { recursive: true });
+    res.status(200).json({ Message: "File Deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ Message: "File Deletion Unsuccessfully" });
+  }
+});
 // update
-app.patch('/files/*dirname', async(req, res)=>{
-    const { dirname } = req.params
-    let filename = dirname.join('/')
-    await rename(`./storage/${filename}`, `./storage/${req.body.newFilename}`)
-    res.json({Message: "File Updated Successfully"})
-})
+app.patch("/files/*dirname", async (req, res) => {
+  const { dirname } = req.params;
+  try {
+      let filename = path.join("/", dirname ? dirname?.join("/") : '');
+      await rename(`./storage/${filename}`, `./storage/${req.body.newFilename}`);
+      res.json({ Message: "File Updated Successfully" });
+  } catch (error) {
+    res.json({error: error.message})
+  }
+});
 
-
-app.listen(port, ()=>{
-    console.log(`Server Listening on http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Listening on http://localhost:${port}`);
+});
